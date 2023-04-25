88dcd43be08e98052e2f5f352989f081166f2221
Edar <108901257+wewtw@users.noreply.github.com>
2023-04-24 23:13:40 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,18 @@ function Home() {
     const link = new HttpLink({ uri: `${API_URL}/graphql`})
     const cache = new InMemoryCache()
     const client = new ApolloClient({link,cache});
+     useEffect(() => {
+    const url = "https://firebasestorage.googleapis.com/v0/b/firetest-bd14e.appspot.com/o/restaurant.png?alt=media&token=568d0157-0054-4171-8200-8f89337cfb0a";
+    const corsImageModified = new Image();
+    corsImageModified.crossOrigin = "Anonymous";
+    corsImageModified.src = url + "?not-from-cache-please";
+    corsImageModified.onload = function() {
+      console.log("CORS image loaded successfully");
+    };
+    corsImageModified.onerror = function() {
+      console.log("Error loading CORS image");
+    };
+  }, []);
     
     const filteredQuery = query.trim().toLocaleLowerCase();
   //last good no errors, test2.
@@ -19,7 +31,7 @@ function Home() {
         <ApolloProvider client={client}>
             <div style={{ backgroundColor: "red", color: "white", padding: "0.5rem" }}>
               <h2>Welcome friend.</h2>
-              <p>Some features of this app like images and receipts are blocked by CORS.<p>
+              <p>Some features of this app like images and receipts are blocked by CORS.</p>
             </div>
           <div className="search" style={{ fontSize: "2rem", marginBottom: "2rem", marginTop: "1rem" }}>
               <h2>Restaurants</h2>
