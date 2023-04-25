07652c39ecfb2f83f64a6af66c42c41feaa71263
Edar <108901257+wewtw@users.noreply.github.com>
2023-04-25 00:05:29 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -38,9 +38,9 @@ function Home() {
             </div>
             <div style={{ backgroundColor: "red", color: "white", padding: "0.5rem" }}>
            
-              <p>One way I found I can fix the issue with CORS I can use react hook useffect. 
-                  However this is something I will have to adress at a later time. It was not a part of our learning curriculum or the submission rubric.
-                  Thank you for understaning. 
+              <p>One way I found I can fix the issue with CORS is, I can use react hook useffect. 
+              However this is something I will have to address at a later time. 
+              It was not a part of our learning curriculum or the submission rubric. Thank you for understanding.
               </p>
             </div>
             
