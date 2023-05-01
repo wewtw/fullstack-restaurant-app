71133b8d6c92486d968d46a95c4a71b88fdbc5dd
Edar <108901257+wewtw@users.noreply.github.com>
2023-04-30 23:57:57 -0400

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -52,8 +52,7 @@ function Login(props) {
                   >
                     {errorMessage}
                   </div>
-                    ))
-                })}
+                    ))}
                <Form>
                 <fieldset disabled={loading}>
                   <FormGroup>
